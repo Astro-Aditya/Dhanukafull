07700b3211ac8f94bcbd953e22d41d0deff93584
DeadPool9667 <dead.pool9667@gmail.com>
2022-12-06 14:50:26 +0530

    second commit

--- Frontend/src/components/url.js
+++ Frontend/src/components/url.js
@@ -1,3 +1,4 @@
 // export const URL = 'https://dhanuka-backend.herokuapp.com'
 // export const URL = 'http://localhost:5000'
 // export const URL = 'http//ec2-3-86-196-7.compute-1.amazonaws.com'
+export const URL = 'https://dead-erin-pelican-shoe.cyclic.app'
